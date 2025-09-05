9f97425841b4e4016fde948a3e9a5337b6efcc8a
Stephanie Gogarten <sdmorris@uw.edu>
2025-09-05 22:16:46 +0000

    make demo more general

--- legacy_project/PRIMED_demo_clusters.R
+++ legacy_project/PRIMED_demo_clusters.R
@@ -4,22 +4,22 @@ remotes::install_github("UW-GAC/prsmixsumstats")
 library(prsmixsumstats)
 
 cohort <- "ARIC"
+trait <- "WBC"
+bucket <- "gs://fc-a8511200-791a-4375-bccf-fbe41ac3f9f6/summary_stats/"
 
 # each of these files should have sample identifier as the first column
-trait <- read_tsv(paste0(cohort, "_WBC_trait.tsv"))
-covariates <- read_tsv(paste0(cohort, "_WBC_covariates.tsv"))
-scores <- read_tsv(paste0(cohort, "_adjusted_scores_filtered.tsv"))
+# all other columns should follow naming convention here:
+# https://docs.google.com/spreadsheets/d/1B_AUoAlq9dY4GDxv2YgeE_tfDspqn2wJBD5N_P_fZYg/edit?usp=sharing
+trait <- read_tsv(paste(cohort, trait, "trait.tsv", sep="_"))
+covariates <- read_tsv(paste(cohort, trait, "covariates.tsv", sep="_"))
+scores <- read_tsv(paste0(cohort, "_adjusted_scores.tsv"))
 clusters <- read_tsv(paste0(cohort, "_cluster_definitions.tsv"))
 
-make_sumstats_clusters(trait, covariates, scores, clusters, trait_name="WBC", cohort_name=cohort)
-avcopy(list.files(pattern="_sumstats.rds$"), avstorage())
+make_sumstats_clusters(trait, covariates, scores, clusters, trait_name=trait, cohort_name=cohort)
+avcopy(list.files(pattern="_sumstats.rds$"), bucket)
 
-sumst <- readRDS(paste0("WBC_", cohort, "_sumstats.rds"))
-dim(sumst$xx)
-head(sumst$xy)
-attr(sumst, "nsubj")
-attr(sumst, "nmiss")
-attr(sumst, "ysum")
-attr(sumst, "yssq")
+# check
+sumst <- readRDS(paste(trait, cohort, "sumstats.rds", sep="_"))
+str(sumst)
 
 
