7f5eb61c6c2ba81750e0cc3d514fd84818411df2
Stephanie Gogarten <sdmorris@uw.edu>
2025-09-24 19:59:23 +0000

    add link to example files

--- legacy_project/PRIMED_demo_clusters.R
+++ legacy_project/PRIMED_demo_clusters.R
@@ -10,6 +10,8 @@ bucket <- "gs://fc-a8511200-791a-4375-bccf-fbe41ac3f9f6/summary_stats/"
 # each of these files should have sample identifier as the first column
 # all other columns should follow naming convention here:
 # https://docs.google.com/spreadsheets/d/1B_AUoAlq9dY4GDxv2YgeE_tfDspqn2wJBD5N_P_fZYg/edit?usp=sharing
+# example files are here:
+# https://github.com/UW-GAC/prsmixsumstats/tree/main/inst/extdata
 trait <- read_tsv(paste(cohort, analysis_name, "trait.tsv", sep="_"))
 covariates <- read_tsv(paste(cohort, analysis_name, "covariates.tsv", sep="_"))
 scores <- read_tsv(paste0(cohort, "_adjusted_scores.tsv"))
