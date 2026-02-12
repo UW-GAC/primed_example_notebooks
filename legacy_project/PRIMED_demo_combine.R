599d3648b9eab371ed63090f2cf1240d1d63e002
Stephanie Gogarten <sdmorris@uw.edu>
2026-02-12 00:39:59 +0000

    update to filtering scores

--- legacy_project/PRIMED_demo_combine.R
+++ legacy_project/PRIMED_demo_combine.R
@@ -1,27 +1,28 @@
 library(AnVIL)
 library(readr)
-remotes::install_github("UW-GAC/prsmixsumstats")
+#remotes::install_github("UW-GAC/prsmixsumstats")
 library(prsmixsumstats)
 
-this_trait <- "WBC"
+#this_trait <- "WBC"
+this_trait <- "CAD_subcovar"
 sumst_tbl <- avtable("sumstats") %>%
   filter(analysis == this_trait)
 
-aou_file <- "gs://fc-a8511200-791a-4375-bccf-fbe41ac3f9f6/AoU_pgs_id.csv"
-#avcopy(aou_file, ".")
-aou_scores <- stringr::str_trim(readLines(basename(aou_file)))
+keep_scores <- readLines("harmonized_scores_noAoU_noexcl.txt")
 
 dest_bucket <- file.path(avstorage(), "pgs_sumstats/")
 data_dir <- "~/pgs_sumstats"
 date_str <- format(Sys.Date(), "%Y%m%d")
 clusters <- unique(sumst_tbl$cluster)
+#clusters <- "all"
+
 tbl_list <- list()
 for (clust in clusters) {
   this <- sumst_tbl %>%
     filter(cluster == clust) 
   files <- this %>%
     select(sumstat_file, overlap_file)
-  for (f in unlist(files)) {
+  for (f in unique(unlist(files))) {
     local_file <- file.path(data_dir, basename(f))
     if (!file.exists(local_file)) suppressWarnings(avcopy(f, local_file))
   }
@@ -36,8 +37,9 @@ for (clust in clusters) {
   }
   sumst_comb <- combine_sumstats(sumst_list)
   
-  # remove AoU scores
-  sumst_comb$sumstats <- drop_cols_sumstats(sumst_comb$sumstats, aou_scores)
+  # remove AoU and excluded scores
+  cols_to_drop <- setdiff(colnames(sumst_comb$sumstats$xx), keep_scores)
+  sumst_comb$sumstats <- drop_cols_sumstats(sumst_comb$sumstats, cols_to_drop)
   
   # check that we are not missing any covariates
   stopifnot(all(str_detect(sumst_comb$incomplete_cols, "^PGS")))
@@ -50,7 +52,7 @@ for (clust in clusters) {
   tbl_list[[clust]] <- tibble(
     analysis = this_trait,
     cluster = clust,
-    cohorts = paste(sort(this$cohort), collapse="|"),
+    biobanks = paste(sort(unique(this$biobank)), collapse="|"),
     combined_sumstats = paste0(dest_bucket, basename(outfile)),
     analysis_id = paste(this_trait, cluster, date_str, sep="_")
   )
