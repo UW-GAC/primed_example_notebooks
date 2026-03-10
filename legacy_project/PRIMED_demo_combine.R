67847d4231b33d47938a8d44b838fc7432439005
Stephanie Gogarten <sdmorris@uw.edu>
2026-03-10 21:24:28 +0000

    set penalty factor, allow adjusted or unadjusted

--- legacy_project/PRIMED_demo_combine.R
+++ legacy_project/PRIMED_demo_combine.R
@@ -1,22 +1,31 @@
 library(AnVIL)
 library(readr)
-#remotes::install_github("UW-GAC/prsmixsumstats")
+#remotes::install_github("UW-GAC/prsmixsumstats", ref="rename")
 library(prsmixsumstats)
 
-#this_trait <- "WBC"
-this_trait <- "CAD_subcovar"
-sumst_tbl <- avtable("sumstats") %>%
-  filter(analysis == this_trait)
+adjusted <- FALSE
+this_trait <- "TG"
+validation <- "GeneSTAR"
+drop_cohorts <- c("eMERGE-Geisinger", "eMERGE-KPWUW", "eMERGE-MountSinai")
+
+if (adjusted) id_str <- "analysis_adjusted_id" else file_str <- "analysis_unadjusted_id"
+if (adjusted) file_str <- "sumstats_adjusted" else file_str <- "sumstats_unadjusted"
+
+sumst_tbl <- avtable(file_str) %>%
+  filter(analysis == this_trait) %>%
+  filter(biobank != validation) %>%
+  filter(!(cohort %in% drop_cohorts))
 
 drop_scores <- readLines("drop_scores_AoU_excl.txt")
 
-dest_bucket <- file.path(avstorage(), "pgs_sumstats/")
-data_dir <- "~/pgs_sumstats"
+dest_bucket <- paste0(avstorage(), "/pgs_", file_str, "/")
+data_dir <- paste0("~/pgs_", file_str)
 date_str <- format(Sys.Date(), "%Y%m%d")
-clusters <- unique(sumst_tbl$cluster)
+clusters <- sort(unique(sumst_tbl$cluster))
 
 tbl_list <- list()
 for (clust in clusters) {
+  print(clust)
   this <- sumst_tbl %>%
     filter(cluster == clust) 
   files <- this %>%
@@ -28,11 +37,22 @@ for (clust in clusters) {
   
   sumst_list <- list()
   for (i in 1:nrow(this)) {
+    print(this[[paste0(file_str, "_id")]][i])
     sumst <- readRDS(file.path(data_dir, basename(this$sumstat_file[i])))
+    covars <- colnames(sumst$xx)[!grepl("^PGS", colnames(sumst$xx))]
+    print(sort(covars))
+    # rename columns with incorrect names
+    if ("bmi" %in% colnames(sumst$xx)) {
+      sumst <- rename_col_sumstats(sumst, "bmi", "BMI")
+    }
     overlap <- read_tsv(file.path(data_dir, basename(this$overlap_file[i])), show_col_types=FALSE)
-    sumst_list[[i]] <- filter_sumstats(sumst, overlap, name_col="score", 
+    sumst <- filter_sumstats(sumst, overlap, name_col="score", 
                              filter_col="beta_fraction", 
-                             filter_threshold=0.8)
+                             filter_threshold=0.75)
+    sumst <- filter_sumstats(sumst, overlap, name_col="score", 
+                             filter_col="overlap", 
+                             filter_threshold=0.75)
+    sumst_list[[i]] <- sumst
   }
   sumst_comb <- combine_sumstats(sumst_list)
   
@@ -42,23 +62,42 @@ for (clust in clusters) {
   stopifnot(all(colnames(sumst_comb$sumstats$xx) == names(sumst_comb$beta_multiplier)))
   
   # check that we are not missing any covariates
-  stopifnot(all(str_detect(sumst_comb$incomplete_cols, "^PGS")))
+  drop <- sumst_comb$incomplete_cols[!grepl("^PGS", sumst_comb$incomplete_cols)]
+  if (length(drop) > 0) {
+    print("drop:")
+    print(drop)
+    sumst_comb$sumstats <- drop_cols_sumstats(sumst_comb$sumstats, drop)
+    sumst_comb$beta_multiplier <- sumst_comb$beta_multiplier[colnames(sumst_comb$sumstats$xx)]
+    stopifnot(all(colnames(sumst_comb$sumstats$xx) == names(sumst_comb$beta_multiplier)))
+  }
+  
+  covars <- colnames(sumst_comb$sumstats$xx)[!grepl("^PGS", colnames(sumst_comb$sumstats$xx))]
+  print("covars:")
+  print(sort(covars))
+  
+  # penalty factor should be 0 for covariates and 1 for PGS
+  penalty_factor <- rep(0, ncol(sumst_comb$sumstats$xx))
+  names(penalty_factor) <- colnames(sumst_comb$sumstats$xx)
+  penalty_factor[grepl("^PGS", names(penalty_factor))] <- 1
+  sumst_comb$penalty_factor <- penalty_factor
+  
   print(paste("number of incomplete PRS:", length(sumst_comb$incomplete_cols)))
+  print(paste("number of PRS with no variation:", length(sumst_comb$diag_zero_cols)))
   
   print(str(sumst_comb$sumstats))
   print(head(colnames(sumst_comb$sumstats$xx)))
   print(tail(colnames(sumst_comb$sumstats$xx)))
-  outfile <- file.path(data_dir, paste(this_trait, clust, date_str, "sumstats.rds", sep="_"))
+  outfile <- paste0(data_dir, "/", paste(this_trait, clust, date_str, file_str, sep="_"), ".rds")
   saveRDS(sumst_comb, outfile)
   avcopy(outfile, dest_bucket)
   tbl_list[[clust]] <- tibble(
     analysis = this_trait,
     cluster = clust,
     biobanks = paste(sort(unique(this$biobank)), collapse="|"),
-    combined_sumstats = paste0(dest_bucket, basename(outfile)),
-    analysis_id = paste(this_trait, cluster, date_str, sep="_")
+    combined_sumstats = paste0(dest_bucket, basename(outfile))
   )
+  tbl_list[[clust]][[id_str]] <- paste(this_trait, clust, date_str, sep="_")
 }
 
 tbl <- bind_rows(tbl_list)
-avtable_import(tbl, entity="analysis_id")
+avtable_import(tbl, entity=id_str)
