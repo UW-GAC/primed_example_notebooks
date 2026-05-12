55a0f4697056e3bfc1bd026f4d0d2ce37c7720cb
Stephanie Gogarten <sdmorris@uw.edu>
2026-05-12 17:33:13 +0000

    add filtering options, fix missing _SUM in names

--- legacy_project/PRIMED_demo_combine.R
+++ legacy_project/PRIMED_demo_combine.R
@@ -4,19 +4,37 @@ library(readr)
 library(prsmixsumstats)
 
 adjusted <- FALSE
+filter_method <- "none" # options: drop, zero, none
+filter_threshold <- 0
 this_trait <- "TG"
-validation <- "GeneSTAR"
-drop_cohorts <- c("eMERGE-Geisinger", "eMERGE-KPWUW", "eMERGE-MountSinai")
+trait_type <- "binary"
+validation <- c()
+drop_cohorts <- c("eMERGE-KPWUW") # no BMI
+#drop_cohorts <- c("eMERGE-KPWUW", "eMERGE-CHOP") # no LDL
+#drop_cohorts <- c()
 
 id_str <- if (adjusted) "analysis_adjusted_id" else "analysis_unadjusted_id"
 file_str <- if (adjusted) "sumstats_adjusted" else "sumstats_unadjusted"
 
 sumst_tbl <- avtable(file_str) %>%
   filter(analysis == this_trait) %>%
-  filter(biobank != validation) %>%
+  filter(!(biobank %in% validation)) %>%
   filter(!(cohort %in% drop_cohorts))
 
 drop_scores <- readLines("drop_scores_AoU_excl.txt")
+if (!adjusted) drop_scores <- paste0(drop_scores, "_SUM")
+length(drop_scores)
+
+drop_cols <- function(sumst_comb, drop) {
+  nc1 <- ncol(sumst_comb$sumstats$xx)
+  sumst_comb$sumstats <- drop_cols_sumstats(sumst_comb$sumstats, drop)
+  sumst_comb$beta_multiplier <- sumst_comb$beta_multiplier[colnames(sumst_comb$sumstats$xx)]
+  stopifnot(all(colnames(sumst_comb$sumstats$xx) == names(sumst_comb$beta_multiplier)))
+  nc2 <- ncol(sumst_comb$sumstats$xx)
+  print(paste("dropped", nc1 - nc2, "columns"))
+  print(paste("new ncol:", nc2))
+  return(sumst_comb)
+}
 
 dest_bucket <- paste0(avstorage(), "/pgs_", file_str, "/")
 data_dir <- paste0("~/pgs_", file_str)
@@ -36,39 +54,72 @@ for (clust in clusters) {
   }
   
   sumst_list <- list()
+  drop_list <- list()
   for (i in 1:nrow(this)) {
     print(this[[paste0(file_str, "_id")]][i])
     sumst <- readRDS(file.path(data_dir, basename(this$sumstat_file[i])))
     covars <- colnames(sumst$xx)[!grepl("^PGS", colnames(sumst$xx))]
     print(sort(covars))
+    pgs <- colnames(sumst$xx)[grepl("^PGS", colnames(sumst$xx))]
+    print(head(pgs))
     # rename columns with incorrect names
     if ("bmi" %in% colnames(sumst$xx)) {
       sumst <- rename_col_sumstats(sumst, "bmi", "BMI")
     }
-    overlap <- read_tsv(file.path(data_dir, basename(this$overlap_file[i])), show_col_types=FALSE)
-    sumst <- filter_sumstats(sumst, overlap, name_col="score", 
-                             filter_col="beta_fraction", 
-                             filter_threshold=0.75)
-    sumst <- filter_sumstats(sumst, overlap, name_col="score", 
-                             filter_col="overlap", 
-                             filter_threshold=0.75)
+    if ("Age" %in% colnames(sumst$xx)) {
+      sumst <- rename_col_sumstats(sumst, "Age", "age")
+    }
+    if ("Age2" %in% colnames(sumst$xx)) {
+      sumst <- rename_col_sumstats(sumst, "Age2", "age2")
+    }
+    if ("Sex" %in% colnames(sumst$xx)) {
+      sumst <- rename_col_sumstats(sumst, "Sex", "sex")
+    }
+    if (!adjusted) {
+      # check that scores end with "_SUM"
+      for (p in pgs) {
+        if (!grepl("_SUM$", p)) {
+          sumst <- rename_col_sumstats(sumst, p, paste0(p, "_SUM"))
+        }
+      }
+    }
+    if (filter_method != "none") {
+      overlap <- read_tsv(file.path(data_dir, basename(this$overlap_file[i])), show_col_types=FALSE)
+      if (!adjusted) overlap$score <- paste0(overlap$score, "_SUM")
+    }
+    if (filter_method == "zero") {
+      message("zeroing scores")
+      sumst <- filter_sumstats(sumst, overlap, name_col="score", 
+                               filter_col="beta_fraction", 
+                               filter_threshold=filter_threshold)
+      sumst <- filter_sumstats(sumst, overlap, name_col="score", 
+                               filter_col="overlap", 
+                               filter_threshold=filter_threshold)
+    } else if (filter_method == "drop") {
+      drop_list[[i]] <- overlap %>%
+        filter(overlap < filter_threshold | beta_fraction < filter_threshold) %>%
+        select(score) %>%
+        unlist()
+    }
     sumst_list[[i]] <- sumst
   }
   sumst_comb <- combine_sumstats(sumst_list)
   
   # remove AoU and excluded scores
-  sumst_comb$sumstats <- drop_cols_sumstats(sumst_comb$sumstats, drop_scores)
-  sumst_comb$beta_multiplier <- sumst_comb$beta_multiplier[colnames(sumst_comb$sumstats$xx)]
-  stopifnot(all(colnames(sumst_comb$sumstats$xx) == names(sumst_comb$beta_multiplier)))
+  sumst_comb <- drop_cols(sumst_comb, drop_scores)
+  
+  # remove scores that fail overlap filter
+  if (filter_method == "drop") {
+    drop_overlap <- unique(unlist(drop_list, use.names=FALSE))
+    sumst_comb <- drop_cols(sumst_comb, drop_overlap)
+  }
   
   # check that we are not missing any covariates
   drop <- sumst_comb$incomplete_cols[!grepl("^PGS", sumst_comb$incomplete_cols)]
   if (length(drop) > 0) {
     print("drop:")
     print(drop)
-    sumst_comb$sumstats <- drop_cols_sumstats(sumst_comb$sumstats, drop)
-    sumst_comb$beta_multiplier <- sumst_comb$beta_multiplier[colnames(sumst_comb$sumstats$xx)]
-    stopifnot(all(colnames(sumst_comb$sumstats$xx) == names(sumst_comb$beta_multiplier)))
+    sumst_comb <- drop_cols(sumst_comb, drop)
   }
   
   covars <- colnames(sumst_comb$sumstats$xx)[!grepl("^PGS", colnames(sumst_comb$sumstats$xx))]
@@ -87,16 +138,19 @@ for (clust in clusters) {
   print(str(sumst_comb$sumstats))
   print(head(colnames(sumst_comb$sumstats$xx)))
   print(tail(colnames(sumst_comb$sumstats$xx)))
-  outfile <- paste0(data_dir, "/", paste(this_trait, clust, date_str, file_str, sep="_"), ".rds")
+  outfile <- paste0(data_dir, "/", paste(this_trait, clust, filter_method, date_str, file_str, sep="_"), ".rds")
   saveRDS(sumst_comb, outfile)
   avcopy(outfile, dest_bucket)
   tbl_list[[clust]] <- tibble(
     analysis = this_trait,
+    trait_type = trait_type,
     cluster = clust,
+    filter_method = filter_method,
+    filter_threshold = filter_threshold,
     biobanks = paste(sort(unique(this$biobank)), collapse="|"),
     combined_sumstats = paste0(dest_bucket, basename(outfile))
   )
-  tbl_list[[clust]][[id_str]] <- paste(this_trait, clust, date_str, sep="_")
+  tbl_list[[clust]][[id_str]] <- paste(this_trait, clust, filter_method, date_str, sep="_")
 }
 
 tbl <- bind_rows(tbl_list)
