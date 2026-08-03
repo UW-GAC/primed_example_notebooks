31df55c30d7d3d2c60e6dcbabd7511f608e0f2e0
Stephanie Gogarten <sdmorris@uw.edu>
2026-08-03 20:54:33 +0000

    add script to count samples in age and sex strata

---
+++ legacy_project/age_sex_counts.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(readr)
+
+trait <- read_tsv("ARIC_T2D_trait.tsv")
+covs <- read_tsv("ARIC_T2D_covariates.tsv")
+
+age_cat_fn <- function(age) {
+  cut(age,
+      breaks = c(-Inf, 25, seq(30, 75, by = 5), Inf),
+      labels = c(
+        "<=25",
+        ">25 to <=30",
+        ">30 to <=35",
+        ">35 to <=40",
+        ">40 to <=45",
+        ">45 to <=50",
+        ">50 to <=55",
+        ">55 to <=60",
+        ">60 to <=65",
+        ">65 to <=70",
+        ">70 to <=75",
+        ">75"
+      ),
+      right = TRUE,      # intervals are (a, b]
+      include.lowest = TRUE,
+      ordered_result = TRUE
+  )
+}
+
+tbl <- trait %>%
+  left_join(covs) %>%
+  mutate(age_cat = age_cat_fn(age)) %>%
+  mutate(T2D=as.factor(T2D), sex=as.factor(sex)) %>%
+  count(age_cat, T2D, sex, .drop=FALSE)
+
+write_tsv(tbl, "ARIC_T2D_age_sex_counts.tsv")
